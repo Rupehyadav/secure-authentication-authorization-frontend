b478c74386d9b47a71f716eaf116e6d108884a68
Bren Saud <brensaud4@gmail.com>
2024-09-25 21:17:56 +0545

    Add self signed certificate

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'pems/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'pems/cert.pem')),
+    },
+  },
+});
